64288d597fd98ef42c340e874ea8cd1d7157091f
Aluizio Nunes <aluizio_jr@hotmail.com>
2025-10-20 21:32:31 -0400

    AUTOMAÇÃO - NOVO DEPLOY (AJUSTE DE CONTAINERS - PUBLICAÇÃO DE PORTAS)

--- Backend/NestJS/src/main.ts
+++ Backend/NestJS/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  const port = +(process.env.PORT || 3002);
+  const port = +(process.env.PORT || 3000);
   await app.listen(port);
   console.log(`Core API is running on port ${port}`);
 }
